1110d71eaab569250a0e9069414ff762aa480b90
Rico <rico@test.com>
2021-03-30 08:33:39 +0200

    ready for fist deploy test

--- app.js
+++ app.js
@@ -76,4 +76,10 @@ app.get("/post/:postTitle", function(req, res) {
 });
 });
 
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port, function(){
+  console.log("Server started on port 8000");
+});
