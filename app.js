bcc8411c86985814276d5bf1cd0ac67f47581d1d
Rico <rico@test.com>
2021-04-04 13:30:33 +0200

    connect with MongoDB Atlas success

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
 const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -17,17 +17,35 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// Connect with MongoDB Atlas
+mongoose.connect("mongodb+srv://RicoN:test12345@cluster0.g1jhf.mongodb.net/RicoPersonalWebsite?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
-//  All blog posts
+// Create DB schema
+const blogSchema = {
+  title: {
+    type: String,
+    required: [true, "You need to add a a title."]
+  },
+  content: String
+};
 
-let allPosts = [];
+// Add Mongoose model based on this schema
+const Blog = mongoose.model("blogscollection", blogSchema);
+
+
+// //  All blog posts
+
+// let allPosts = [];
 
 // Render pages
 
 app.get("/", function(req, res) {
-  res.render("home", {
-    firstParagraph: homeStartingContent, blogContents: allPosts
-  }); 
+
+  Blog.find({}, function(err, blogs){
+    res.render("home", {
+      firstParagraph: homeStartingContent, blogContents: blogs
+    }); 
+  })
 });
 
 app.get("/about", function(req, res) {
@@ -49,31 +67,31 @@ app.get("/compose", function(req, res) {
 // Receive Post blog post
 
 app.post("/compose", function (req, res) {
-  const newPost = {
+  const newBlog = new Blog ({
     title: req.body.postTitleBlog,
-    content: req.body.postBlog
-  }
-  allPosts.push(newPost);
+    content: req.body.postContentBlog
+  });
   
-  res.redirect("/");
+  newBlog.save(function(err){
+    if (!err){
+      res.redirect("/");
+    }
+  });
+
 });
 
 // Get individual posts - per page
 
-app.get("/post/:postTitle", function(req, res) {
-  const requestedTitle = _.lowerCase(req.params.postTitle);
+app.get("/posts/:postId", function(req, res) {
+  const requestedPostId = req.params.postId;
 
-  allPosts.forEach(function(newPost) {
-    const storedTitle = _.lowerCase(newPost.title);
-
-  if (storedTitle === requestedTitle) {
-
+  Blog.findOne({_id: requestedPostId}, function(err, newPost){
     res.render("post", {
-      individualPostTitle: newPost.title, individualPostContent: newPost.content
+      title: newPost.title,
+      content: newPost.content
     });
+  });
 
-  };
-});
 });
 
 let port = process.env.PORT;
